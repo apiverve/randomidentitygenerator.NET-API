ef709085f69b5a013e36e99a65941a735484d332
APIVerve Team <hello@apiverve.com>
2024-05-14 15:38:36 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,63 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class company
+{
+    [JsonProperty("name")]
+    public string name { get; set; }
+
+    [JsonProperty("catchPhrase")]
+    public string catchPhrase { get; set; }
+
+    [JsonProperty("bs")]
+    public string bs { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("name")]
+    public string name { get; set; }
+
+    [JsonProperty("username")]
+    public string username { get; set; }
+
+    [JsonProperty("email")]
+    public string email { get; set; }
+
+    [JsonProperty("phone")]
+    public string phone { get; set; }
+
+    [JsonProperty("website")]
+    public string website { get; set; }
+
+    [JsonProperty("company")]
+    public company company { get; set; }
+
+    [JsonProperty("gender")]
+    public string gender { get; set; }
+
+    [JsonProperty("birthdate")]
+    public string birthdate { get; set; }
+
+    [JsonProperty("cell")]
+    public string cell { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
